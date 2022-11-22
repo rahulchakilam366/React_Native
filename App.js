0874026ff5514e9932f73beea3601a501013cd20
Sai Varma <sai.varma@3pillarglobal.com>
2022-11-22 12:34:11 -0500

    change commit

--- App.js
+++ App.js
@@ -85,9 +85,7 @@ const App: () => Node = () => {
           <Section title="Debug">
             <DebugInstructions />
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
+
           <LearnMoreLinks />
         </View>
       </ScrollView>
